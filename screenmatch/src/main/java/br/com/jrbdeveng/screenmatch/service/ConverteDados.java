92569f2c1e1108aa73d3ed4abf3107a2cd80e9bc
joiicebatista <joiicebatista123@gmail.com>
2025-12-15 10:35:37 -0300

    start spring project

---
+++ screenmatch/src/main/java/br/com/jrbdeveng/screenmatch/service/ConverteDados.java
@@ -0,0 +1,14 @@
+package br.com.jrbdeveng.screenmatch.service;
+
+import br.com.jrbdeveng.screenmatch.model.DadosSerie;
+import tools.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados{
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        return mapper.readValue(json, classe);
+    }
+}
